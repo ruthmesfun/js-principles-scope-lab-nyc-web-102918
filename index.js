105087395751d7b92e570ea6b038c33b1be4ef21
ruthmesfun <ruth.mesfun@gmail.com>
2018-12-07 21:47:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+let upperCaseCustomerName = () => {
+    customerName = 'BOB'
+}
+
+let setBestCustomer = () => {
+    bestCustomer = 'not bob'
+}
+
+let overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'eden'
+
+let changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'ruth'
+}
